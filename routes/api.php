81568502f9080b610df15d22e70763fe79bd696e
BayuFebriyono <bayu.f.p.09@gmail.com>
2024-04-09 17:59:33 +0700

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::get('user', [AuthController::class, 'me']);
+Route::post('logout', [AuthController::class, 'logout']);
